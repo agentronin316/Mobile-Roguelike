42feb48eb85e97a70eab596443be635f36f7e75b
agentronin316 <agentronin316@gmail.com>
2016-02-25 16:53:55 -0600

    Touch Gesture Homework
    This guy really does not know his math.

---
+++ Roguelike/Assets/Scripts/OneFingerGestures.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OneFingerGestures : MonoBehaviour {
+
+    public float swipeMinLength = 3f;
+
+    GameObject movingObject;
+    
+    Vector2 start;
+    Vector2 end;
+    Vector2 swipe;
+
+    // Update is called once per frame
+    void Update ()
+    {
+	    if (Input.touchCount == 1)
+        {
+            if (Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                start = Input.GetTouch(0).position;
+            }
+            else if (Input.GetTouch(0).phase == TouchPhase.Ended)
+            {
+                end = Input.GetTouch(0).position;
+                swipe = end - start;
+                if (swipe.magnitude >= swipeMinLength)
+                {
+                    Debug.Log("Swipe Detected");
+                    if (swipe.x > swipe.y)
+                    {
+                        //Down or Right
+                        if (swipe.x > -(swipe.y))
+                        {
+                            Debug.Log("Swipe Right");
+                        }
+                        else
+                        {
+                            Debug.Log("Swipe Down");
+                        }
+                    }
+                    else
+                    {
+                        //Up or Left
+                        if (swipe.x > -(swipe.y))
+                        {
+                            Debug.Log("Swipe Up");
+                        }
+                        else
+                        {
+                            Debug.Log("Swipe Left");
+                        }
+                    }
+                }
+                else if (Input.GetTouch(0).tapCount == 1)
+                {
+                    //Debug.Log("Single Tap");
+                    RaycastHit hit;
+                    Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+                    if (Physics.Raycast(ray, out hit, 10f))
+                    {
+                        movingObject = hit.collider.gameObject;
+                    }
+                }
+                else if (Input.GetTouch(0).tapCount == 2)
+                {
+                    Debug.Log("Double Tap");
+                    if (movingObject != null)
+                    {
+                        Vector3 pos = new Vector3();
+                        pos.x = Input.GetTouch(0).position.x;
+                        pos.y = Input.GetTouch(0).position.y;
+                        pos.z = Mathf.Abs(Camera.main.transform.position.z - movingObject.transform.position.z);
+
+                        movingObject.transform.position = Camera.main.ScreenToWorldPoint(pos);
+                    }
+                }
+            }
+        }
+	}
+}
---
+++ Roguelike/Assets/Scripts/TwoFingerGestures.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TwoFingerGestures : MonoBehaviour {
+
+    public GameObject toRotate;
+    public float zoomSensitivity;
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(Input.touchCount == 2 && Input.GetTouch(0).phase == TouchPhase.Moved && Input.GetTouch(1).phase == TouchPhase.Moved)
+        {
+            Vector2 thisFrame = Input.GetTouch(0).position - Input.GetTouch(1).position;
+            Vector2 lastFrame = thisFrame + Input.GetTouch(1).deltaPosition - Input.GetTouch(0).deltaPosition;
+
+            float touchDelta = thisFrame.magnitude - lastFrame.magnitude;
+            if (touchDelta > zoomSensitivity)
+            {
+                Debug.Log("zoom in");
+                Camera.main.fieldOfView -= touchDelta;
+            }
+            else if (touchDelta < -zoomSensitivity)
+            {
+                Debug.Log("zoom out");
+                Camera.main.fieldOfView -= touchDelta;
+
+            }
+
+
+        }
+	}
+}
