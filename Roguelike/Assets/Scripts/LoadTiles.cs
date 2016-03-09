bb576bc77675b8dfb964bd71eff983937f825fac
agentronin316 <agentronin316@gmail.com>
2016-03-08 19:08:55 -0600

    Added import for Tiled properties
    Why do they do it that way?

--- Roguelike/Assets/Scripts/LoadTiles.cs
+++ Roguelike/Assets/Scripts/LoadTiles.cs
@@ -1,20 +1,29 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 using System.Xml;
+
+public enum InteractiveProperties
+{
+    STAIRS_UP,
+    STAIRS_DOWN
+}
 
 public class LoadTiles : MonoBehaviour {
 
     public TextAsset[] mapInformation; //holds the .xml file
 
-    public GameObject tempCube; //a temporary tile
+    //public GameObject tempCube; //a temporary tile
 
-    GameObject tileParent;
+    public List<InteractiveProperties> properties = new List<InteractiveProperties>();
+    public List<int> tileIDs = new List<int>();
+
+    GameObject mapParent;
 
     Sprite[] sprites;
 
     void Start()
     {
-        tileParent = new GameObject();
+        mapParent = new GameObject("map");
         LoadMap(1);
     }
 
@@ -26,24 +35,47 @@ public class LoadTiles : MonoBehaviour {
         xmlDoc.LoadXml(mapInformation[index - 1].text);
 
         XmlNodeList tempNodes = xmlDoc.GetElementsByTagName("tileset");
-
+        
         //Debug.Log(tempNodes[0].Attributes.GetNamedItem("name").Value);
         sprites = Resources.LoadAll<Sprite>(tempNodes[0].Attributes.GetNamedItem("name").Value);
-        Debug.Log(sprites.Length);
-        
+        //Debug.Log(sprites.Length);
+
+        tempNodes = tempNodes[0].ChildNodes;
+        foreach (XmlNode node in tempNodes)
+        {
+            if (node.Name == "tile")
+            {
+                XmlNode propertyNode = node.FirstChild.FirstChild;
+                while (propertyNode != null)
+                {
+                    if(propertyNode.Attributes.GetNamedItem("name").Value.ToUpper() == "STAIRSUP")
+                    {
+                        properties.Add(InteractiveProperties.STAIRS_UP);
+                    }
+                    else if(propertyNode.Attributes.GetNamedItem("name").Value.ToUpper() == "STAIRSDOWN")
+                    {
+                        properties.Add(InteractiveProperties.STAIRS_DOWN);
+                    }
+                    tileIDs.Add(int.Parse(node.Attributes.GetNamedItem("id").Value));
+                    propertyNode = propertyNode.NextSibling;
+                }
+
+            }
+        }
+
 
         Camera.main.transform.position = new Vector3(9.9f, -9.9f, 10f);
         Camera.main.transform.rotation = Quaternion.Euler(0f, 180f, 0f);
 
         XmlNodeList layerNames = xmlDoc.GetElementsByTagName("layer");
-        Debug.Log(layerNames.Count);
+        //Debug.Log(layerNames.Count);
 
-        foreach (XmlNode layerInfo in layerNames)
-        {
-            Debug.Log("name: " + layerInfo.Attributes.GetNamedItem("name").InnerText);
-            Debug.Log("width: " + layerInfo.Attributes.GetNamedItem("width").InnerText);
-            Debug.Log("height: " + layerInfo.Attributes.GetNamedItem("height").InnerText);
-        }
+        //foreach (XmlNode layerInfo in layerNames)
+        //{
+        //    Debug.Log("name: " + layerInfo.Attributes.GetNamedItem("name").InnerText);
+        //    Debug.Log("width: " + layerInfo.Attributes.GetNamedItem("width").InnerText);
+        //    Debug.Log("height: " + layerInfo.Attributes.GetNamedItem("height").InnerText);
+        //}
 
         XmlNode tilesetInfo = xmlDoc.SelectSingleNode("map").SelectSingleNode("tileset");
 
@@ -55,18 +87,8 @@ public class LoadTiles : MonoBehaviour {
             int layerWidth = int.Parse(layerInfo.Attributes.GetNamedItem("width").InnerText);
             int layerHeight = int.Parse(layerInfo.Attributes.GetNamedItem("height").InnerText);
 
-            switch(layerInfo.Attributes.GetNamedItem("name").InnerText)
-            {
-                case "Obstacles":
-
-                    goto default;
-                case "Interactive":
-
-                    goto default;
-                default:
-                    ParseLayer(layerInfo, layerWidth, layerHeight, tileWidth, tileHeight);
-                    break;
-            }
+            ParseLayer(layerInfo, layerWidth, layerHeight, tileWidth, tileHeight);
+            
         }
 
     }
@@ -74,7 +96,8 @@ public class LoadTiles : MonoBehaviour {
     void ParseLayer(XmlNode layerInfo, float layerWidth, float layerHeight, float tileWidth, float tileHeight)
     {
         XmlNode tempNode = layerInfo.SelectSingleNode("data");
-
+        GameObject layerParent = new GameObject(layerInfo.Attributes.GetNamedItem("name").Value);
+        layerParent.transform.parent = mapParent.transform;
         int mapLocVert, mapLocHoriz;
         mapLocHoriz = 0;
         mapLocVert = 0;
@@ -85,7 +108,9 @@ public class LoadTiles : MonoBehaviour {
 
             if (spriteValue > 0)
             {
-                GameObject tempSprite = new GameObject("test");
+                string spriteName = string.Format("{0} {1} {2}", layerInfo.Attributes.GetNamedItem("name").InnerText, mapLocHoriz, mapLocVert);
+                GameObject tempSprite = new GameObject(spriteName);
+                tempSprite.transform.parent = layerParent.transform;
                 SpriteRenderer renderer = tempSprite.AddComponent<SpriteRenderer>();
                 float locationX = tileWidth * mapLocHoriz;
                 float locationY = tileHeight * mapLocVert;
@@ -93,6 +118,21 @@ public class LoadTiles : MonoBehaviour {
                 renderer.sprite = sprites[spriteValue - 1];
 
                 renderer.sortingLayerName = layerInfo.Attributes.GetNamedItem("name").InnerText;
+                switch (layerInfo.Attributes.GetNamedItem("name").InnerText)
+                {
+                    case "Obstacles": //Set Obstacle layer tiles to have a collider and a rigidbody
+                        tempSprite.AddComponent<BoxCollider2D>();
+                        tempSprite.AddComponent<Rigidbody2D>();
+                        Rigidbody2D tempRB = tempSprite.GetComponent<Rigidbody2D>();
+                        tempRB.gravityScale = 0f;
+                        tempRB.constraints = RigidbodyConstraints2D.FreezeAll;
+                        break;
+                    case "Interactive": //Set interactive layer tiles to have a trigger collider
+                        tempSprite.AddComponent<BoxCollider2D>();
+                        tempSprite.GetComponent<BoxCollider2D>().isTrigger = true;
+                        //Insert stuff for parsing properties to an enum
+                        break;
+                }
             }
             mapLocHoriz++;
 
