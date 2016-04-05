8d651a095b950183226394377a39e398e404a5c2
agentronin316 <agentronin316@gmail.com>
2016-04-05 18:13:52 -0500

    Menu Transitions Programmed
    Working

--- Roguelike/Assets/Scripts/SwipeMenu.cs
+++ Roguelike/Assets/Scripts/SwipeMenu.cs
@@ -15,11 +15,14 @@ public class SwipeMenu : MonoBehaviour {
     public float swipeMinLength;
 
     MenuState curMenu = MenuState.MAIN;
+    MenuState lastMenu;
+    MenuState nextMenu;
+    Vector2 newMenuPos;
 
     public GameObject mainMenu;
     public GameObject settingsMenu;
     public GameObject creditsMenu;
-    public GameObject DirectionsMenu;
+    public GameObject directionsMenu;
 
 	// Update is called once per frame
 	void Update ()
@@ -58,19 +61,40 @@ public class SwipeMenu : MonoBehaviour {
 
     void MenuTransitionRight()
     {
+        newMenuPos = new Vector2(Screen.width * 3f / 2f, Screen.height / 2f);
         switch (curMenu)
         {
             case MenuState.MAIN:
                 //Credits
+                creditsMenu.SetActive(true);
+                creditsMenu.transform.position = newMenuPos;
+                curMenu = MenuState.TRANSITIONING;
+                lastMenu = MenuState.MAIN;
+                nextMenu = MenuState.CREDITS;
+                StartCoroutine(ShiftMenu());
                 break;
             case MenuState.SETTINGS:
                 //Main
+                mainMenu.SetActive(true);
+                mainMenu.transform.position = newMenuPos;
+                curMenu = MenuState.TRANSITIONING;
+                lastMenu = MenuState.SETTINGS;
+                nextMenu = MenuState.MAIN;
+                StartCoroutine(ShiftMenu());
                 break;
             case MenuState.CREDITS:
                 //Directions
+                directionsMenu.SetActive(true);
+                directionsMenu.transform.position = newMenuPos;
+                curMenu = MenuState.TRANSITIONING;
+                lastMenu = MenuState.CREDITS;
+                nextMenu = MenuState.DIRECTIONS;
+                StartCoroutine(ShiftMenu());
                 break;
         }
     }
+
+    
 
     void MenuTransitionLeft()
     {
@@ -78,12 +102,30 @@ public class SwipeMenu : MonoBehaviour {
         {
             case MenuState.MAIN:
                 //Settings
+                settingsMenu.SetActive(true);
+                settingsMenu.transform.position = newMenuPos;
+                curMenu = MenuState.TRANSITIONING;
+                lastMenu = MenuState.MAIN;
+                nextMenu = MenuState.SETTINGS;
+                StartCoroutine(ShiftMenu());
                 break;
             case MenuState.DIRECTIONS:
                 //Credits
+                creditsMenu.SetActive(true);
+                creditsMenu.transform.position = newMenuPos;
+                curMenu = MenuState.TRANSITIONING;
+                lastMenu = MenuState.DIRECTIONS;
+                nextMenu = MenuState.CREDITS;
+                StartCoroutine(ShiftMenu());
                 break;
             case MenuState.CREDITS:
                 //Main
+                mainMenu.SetActive(true);
+                mainMenu.transform.position = newMenuPos;
+                curMenu = MenuState.TRANSITIONING;
+                lastMenu = MenuState.CREDITS;
+                nextMenu = MenuState.MAIN;
+                StartCoroutine(ShiftMenu());
                 break;
         }
     }
@@ -94,12 +136,30 @@ public class SwipeMenu : MonoBehaviour {
         {
             case MenuState.MAIN:
                 //Directions
+                directionsMenu.SetActive(true);
+                directionsMenu.transform.position = newMenuPos;
+                curMenu = MenuState.TRANSITIONING;
+                lastMenu = MenuState.MAIN;
+                nextMenu = MenuState.DIRECTIONS;
+                StartCoroutine(ShiftMenu());
                 break;
             case MenuState.SETTINGS:
                 //Credits
+                creditsMenu.SetActive(true);
+                creditsMenu.transform.position = newMenuPos;
+                curMenu = MenuState.TRANSITIONING;
+                lastMenu = MenuState.SETTINGS;
+                nextMenu = MenuState.CREDITS;
+                StartCoroutine(ShiftMenu());
                 break;
             case MenuState.DIRECTIONS:
                 //Settings
+                settingsMenu.SetActive(true);
+                settingsMenu.transform.position = newMenuPos;
+                curMenu = MenuState.TRANSITIONING;
+                lastMenu = MenuState.DIRECTIONS;
+                nextMenu = MenuState.SETTINGS;
+                StartCoroutine(ShiftMenu());
                 break;
         }
     }
@@ -110,15 +170,74 @@ public class SwipeMenu : MonoBehaviour {
         {
             case MenuState.DIRECTIONS:
                 //Main
+                mainMenu.SetActive(true);
+                mainMenu.transform.position = newMenuPos;
+                curMenu = MenuState.TRANSITIONING;
+                lastMenu = MenuState.DIRECTIONS;
+                nextMenu = MenuState.MAIN;
+                StartCoroutine(ShiftMenu());
                 break;
             case MenuState.SETTINGS:
                 //Directions
+                directionsMenu.SetActive(true);
+                directionsMenu.transform.position = newMenuPos;
+                curMenu = MenuState.TRANSITIONING;
+                lastMenu = MenuState.SETTINGS;
+                nextMenu = MenuState.DIRECTIONS;
+                StartCoroutine(ShiftMenu());
                 break;
             case MenuState.CREDITS:
                 //Settings
+                settingsMenu.SetActive(true);
+                settingsMenu.transform.position = newMenuPos;
+                curMenu = MenuState.TRANSITIONING;
+                lastMenu = MenuState.CREDITS;
+                nextMenu = MenuState.SETTINGS;
+                StartCoroutine(ShiftMenu());
                 break;
         }
     }
 
-
+    IEnumerator ShiftMenu()
+    {
+        GameObject incomingMenu = mainMenu;
+        GameObject outgoingMenu = mainMenu;
+        switch (nextMenu)
+        {
+            case MenuState.CREDITS:
+                incomingMenu = creditsMenu;
+                break;
+            case MenuState.SETTINGS:
+                incomingMenu = settingsMenu;
+                break;
+            case MenuState.DIRECTIONS:
+                incomingMenu = directionsMenu;
+                break;
+        }
+        switch (lastMenu)
+        {
+            case MenuState.CREDITS:
+                outgoingMenu = creditsMenu;
+                break;
+            case MenuState.SETTINGS:
+                outgoingMenu = settingsMenu;
+                break;
+            case MenuState.DIRECTIONS:
+                outgoingMenu = directionsMenu;
+                break;
+        }
+        Vector2 screenCenter = outgoingMenu.transform.position;
+        float time = 0f;
+        while (time < 1f)
+        {
+            time += Time.deltaTime;
+            Vector2 move = incomingMenu.transform.position;
+            incomingMenu.transform.position = Vector2.Lerp(newMenuPos, screenCenter, time);
+            move -= (Vector2)incomingMenu.transform.position;
+            outgoingMenu.transform.Translate((-1f * move));
+            yield return null;
+        }
+        outgoingMenu.SetActive(false);
+        curMenu = nextMenu;
+    }
 }
